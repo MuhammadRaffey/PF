91a1e38b9175adf4fabe404f0569de8161ba8563
Muhammad Raffey <70153209@student.uol.edu.pk>
2024-10-09 05:23:15 +0000

    done 2nd problem properly

--- Lab 03/prob2.cpp
+++ Lab 03/prob2.cpp
@@ -1,23 +1,47 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int firstNum, secondNum, thirdNum;
+
     cout << "Enter Three Numbers: ";
     cin >> firstNum >> secondNum >> thirdNum;
     cout << "\n";
-    if (firstNum < secondNum && firstNum < thirdNum)
+
+    // Check for equality first
+    if (firstNum == secondNum && secondNum == thirdNum)
     {
-        secondNum < thirdNum ? cout << firstNum << " " << secondNum << " " << thirdNum : cout << firstNum << " " << thirdNum << " " << secondNum;
-    }
-    else if (secondNum < firstNum && secondNum < thirdNum)
-    {
-        firstNum < thirdNum ? cout << secondNum << " " << firstNum << " " << thirdNum : cout << secondNum << " " << thirdNum << " " << firstNum;
+        cout << "All Numbers are equal" << endl;
     }
     else
     {
-        firstNum < secondNum ? cout << thirdNum << " " << firstNum << " " << secondNum : cout << thirdNum << " " << secondNum << " " << firstNum;
+        // Find the smallest, middle, and largest number
+        int smallest, middle, largest;
+
+        // Determine the smallest
+        if (firstNum <= secondNum && firstNum <= thirdNum)
+        {
+            smallest = firstNum;
+            middle = (secondNum <= thirdNum) ? secondNum : thirdNum;
+            largest = (secondNum <= thirdNum) ? thirdNum : secondNum;
+        }
+        else if (secondNum <= firstNum && secondNum <= thirdNum)
+        {
+            smallest = secondNum;
+            middle = (firstNum <= thirdNum) ? firstNum : thirdNum;
+            largest = (firstNum <= thirdNum) ? thirdNum : firstNum;
+        }
+        else
+        {
+            smallest = thirdNum;
+            middle = (firstNum <= secondNum) ? firstNum : secondNum;
+            largest = (firstNum <= secondNum) ? secondNum : firstNum;
+        }
+
+        // Print the sorted order
+        cout << "Sorted Numbers: " << smallest << " " << middle << " " << largest << endl;
     }
-    cout << endl;
+
     return 0;
 }
