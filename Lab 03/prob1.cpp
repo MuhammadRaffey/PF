6bfc561b90ae38671697554ea5b6b23f569e9e49
Muhammad Raffey <70153209@student.uol.edu.pk>
2024-10-09 04:36:22 +0000

    made problem 1 output look good

--- Lab 03/prob1.cpp
+++ Lab 03/prob1.cpp
@@ -1,35 +1,53 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
+
 int main()
 {
     int firstNum, secondNum;
+    cout << "=========================================\n";
+    cout << "        Square and Sum Calculator        \n";
+    cout << "=========================================\n";
+
+    // Taking input
     cout << "Enter two numbers: ";
     cin >> firstNum >> secondNum;
+
     int sumOfEven = 0;
     int sumOfSqOdd = 0;
+
+    cout << "\n\nSquares of numbers less than or equal to 10:\n";
+    cout << "--------------------------------------------\n";
+
     for (int i = firstNum; i <= secondNum; i++)
     {
         if (i <= 10)
         {
-            // squrare all numbers less than 10
-            cout << "Square of: " << i << " is:  " << i * i << "\n";
+            // Print squares of numbers less than or equal to 10
+            cout << "Square of " << i << " is: " << i * i << "\n";
         }
+
         if (i % 2 == 0)
         {
-            // even
+            // Sum even numbers
             sumOfEven += i;
         }
         else
         {
-            // odd
-            cout << "\n   "
-                 << i << " is Odd Number." << endl; // output all odd numbers
+            // Print odd numbers and calculate the sum of their squares
+            cout << "\n   " << i << " is an Odd Number." << endl;
+            cout << "   Square of " << i << " is: " << i * i << "\n";
             sumOfSqOdd += i * i;
         }
     }
-    cout << endl;
-    cout << "Sum of even numbers: " << sumOfEven << endl;
-    cout << "Sum of Square of Odd numbers: " << sumOfSqOdd << endl;
+
+    // Print results
+    cout << "\n=========================================\n";
+    cout << "                 Results                 \n";
+    cout << "=========================================\n";
+    cout << "Sum of all even numbers between " << firstNum << " and " << secondNum << ": " << sumOfEven << endl;
+    cout << "Sum of squares of all odd numbers between " << firstNum << " and " << secondNum << ": " << sumOfSqOdd << endl;
+    cout << "=========================================\n";
+
     return 0;
 }
